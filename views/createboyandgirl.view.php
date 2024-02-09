faa91108fc8f66b7ea2730fd84e80384558b461f
MNITCR <108656979+MNITCR@users.noreply.github.com>
2024-02-09 16:08:05 +0700

    Update createboyandgirl.view.php

--- views/createboyandgirl.view.php
+++ views/createboyandgirl.view.php
@@ -22,7 +22,7 @@
             <div class="rounded-l-lg flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" class="text-green-600 fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
             </div>
-            <span id="text-success" class="text-slate-200 font-medium" style="font-family: 'Khmer OS Battambang';"> <!--Some Message--> </span>
+            <span id="text-success" class="text-slate-200 font-medium" style="font-family: 'Khmer OS Battambang';font-size: 15px;"> <!--Some Message--> </span>
         </div>
     </div>
 
@@ -32,7 +32,7 @@
             <div class="rounded-l-lg flex items-center">
                 <svg class="w-[18px] text-red-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" width="20" height="20"><path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path></svg>
             </div>
-            <span id="text-not-success" class="text-slate-200 font-medium" style="font-family: 'Khmer OS Battambang';"> <!--Some Message--> </span>
+            <span id="text-not-success" class="text-slate-200 font-medium" style="font-family: 'Khmer OS Battambang';font-size: 15px;"> <!--Some Message--> </span>
         </div>
     </div>
 
